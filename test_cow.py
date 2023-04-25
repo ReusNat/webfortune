a8d54bde513c53105575137891dbc9e06788ebce
Nathan Reusch <73974845+ReusNat@users.noreply.github.com>
2023-04-25 10:53:45 -0500

    Update test_cow.py

--- test_cow.py
+++ test_cow.py
@@ -13,7 +13,7 @@ def client(app):
 
 def test_cowsay(app, client):
     message = 'hello'
-    res = client.get(f'/cowsay/{message}')
+    res = client.get(f'/cowsay/{message}/')
     assert res.status_code == 200
-    page_output = res.get_data(as_test=True)
+    page_output = res.get_data(as_text=True)
     assert message in page_output
