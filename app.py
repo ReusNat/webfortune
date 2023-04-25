9133a46b7dbf182a9b2861eb075e32214ba2473b
Nathan <reusnat@bvu.edu>
2023-04-25 11:49:42 -0500

    fixed issues... hopefully

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
 )
 
 from os import system
+import subprocess
 
 app = Flask(__name__)
 app.secret_key = b'REPLACE_ME_x#pi*CO0@^z_beep_beep_boop_boop'
@@ -16,13 +17,15 @@ def index():
 
 @app.route('/fortune/')
 def fortune():
-    system('fortune | cowsay > output.txt')
-    output = open('output.txt', 'r').read()
+    # subprocess.check_output captures the output of running the command
+    # so instead of writing to a file like before we can just capture it here
+    output = subprocess.check_output('fortune | cowsay',
+                                     shell=True).decode()
     return '<pre>' + output + '</pre>'
 
 
 @app.route('/cowsay/<message>/')
 def cowsay(message):
-    system(f'cowsay {message}> output.txt')
-    output = open('output.txt', 'r').read()
+    output = subprocess.check_output(f'cowsay {message}',
+                                     shell=True).decode()
     return '<pre>' + output + '</pre>'
