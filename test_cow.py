a7b5266e8a30c9d3572098813793c214c5af2bb7
Nathan <reusnat@bvu.edu>
2023-04-27 09:36:41 -0500

    added pytest and testing fortune

--- test_cow.py
+++ test_cow.py
@@ -1,6 +1,7 @@
 import pytest
 from app import app as flask_app
 
+
 @pytest.fixture
 def app():
     yield flask_app
@@ -13,7 +14,14 @@ def client(app):
 
 def test_cowsay(app, client):
     message = 'hello'
-    res = client.get('/cowsay/%s/' %message)
+    res = client.get(f'/cowsay/{message}/')
     assert res.status_code == 200
     page_output = res.get_data(as_text=True)
     assert message in page_output
+
+
+def test_fortune(app, client):
+    res = client.get('/fortune/')
+    assert res.status_code == 200
+    page_output = res.get_data(as_text=True)
+    assert '(oo)' in page_output
